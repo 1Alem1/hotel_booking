6540ee206176068dbb7ecb9dff39f7800db97cb5
MatiasAC08 <matiasca549@gmail.com>
2025-06-29 23:54:38 -0300

    Update ReservaService.java

--- src/main/java/com/hibernatestandalone/services/ReservaService.java
+++ src/main/java/com/hibernatestandalone/services/ReservaService.java
@@ -14,17 +14,17 @@ import java.util.List;
 
 public class ReservaService extends AbstractService {
 
-    public List<Habitacion> buscarHabitacionesDisponibles(Date fechaInicio, Date fechaFin) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            String hql = "FROM Habitacion h WHERE h.id NOT IN (" +
-                         "SELECT r.habitacion.id FROM Reserva r " +
-                         "WHERE r.fechaInicio <= :fechaFin AND r.fechaFin >= :fechaInicio)";
-            Query<Habitacion> query = session.createQuery(hql, Habitacion.class);
-            query.setParameter("fechaInicio", fechaInicio);
-            query.setParameter("fechaFin", fechaFin);
-            return query.list();
-        }
+    public List<Habitacion> buscarDisponibles(Date fechaInicio, Date fechaFin) {
+    try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        String hql = "FROM Habitacion h WHERE h.id NOT IN (" +
+                     "SELECT r.habitacion.id FROM Reserva r " +
+                     "WHERE r.fechaInicio <= :fechaFin AND r.fechaFin >= :fechaInicio)";
+        Query<Habitacion> query = session.createQuery(hql, Habitacion.class);
+        query.setParameter("fechaInicio", fechaInicio);
+        query.setParameter("fechaFin", fechaFin);
+        return query.list();
     }
+}
     
     public Reserva cargarReserva(Date fechaInicio, Date fechaFin, Habitacion habitacion, Huesped huesped) {
         Transaction tx = null;
